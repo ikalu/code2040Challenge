ccb0592920ed604bb5fb6bd79b1c77b6cfa67178
Ifeanyi <ikalu13@gmail.com>
2014-12-11 04:13:55 -0500

    Challenge registration

--- registration.rb
+++ registration.rb
@@ -1,7 +1,19 @@
 require 'HTTParty'
 
-result = HTTParty.post( "http://challenge.code2040.org/api/register", 
-	:body => { "email"=> "ifkalu@yahoo.com", "github"=> "https://github.com/ikalu/code2040Challenge.git"}.to_json, 
-	:headers => { 'Content-Type' => 'application/json' } )
+class Registration
 
-puts result
+  def register
+    info = HTTParty.post( "http://challenge.code2040.org/api/register", 
+                           :body => { "email"=> "ifkalu@yahoo.com", "github"=> "https://github.com/ikalu/code2040Challenge.git"}.to_json, 
+                           :headers => { 'Content-Type' => 'application/json' } )
+    print(info["result"])
+  end
+
+  def print(someThing)
+    puts someThing
+  end
+
+end
+
+ifeanyi = Registration.new
+ifeanyi.register
