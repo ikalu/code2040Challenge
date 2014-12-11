13e63c4a6cf0e2da255efa5456c9d226ff19affc
Ifeanyi <ikalu13@gmail.com>
2014-12-11 05:27:39 -0500

    Challenge stage 2 -Needle in a haystack

---
+++ hayStack.rb
@@ -0,0 +1,35 @@
+require 'HTTParty'
+
+class Haystack
+
+  def getCollection
+    stack = HTTParty.post( "http://challenge.code2040.org/api/haystack", 
+                          :body => { "token"=> "8EBDqKCWgK"}.to_json, 
+                          :headers => { 'Content-Type' => 'application/json' } )
+
+    stack["result"]
+  end
+
+  def findNeedle
+    collection = getCollection
+    print(collection)
+    haystack = collection["haystack"]
+    needle = collection["needle"]
+    print(haystack.index(needle))
+    haystack.index(needle).to_i
+  end
+
+  def validateNeedle
+    foundNeedle = HTTParty.post( "http://challenge.code2040.org/api/validateneedle",
+                                   :body => { "token" => "8EBDqKCWgK", "needle" => findNeedle }.to_json,
+                                   :headers => { 'Content-Type' => 'application/json' } )
+    print(foundNeedle)
+  end
+
+  def print(someThing)
+    puts someThing
+  end
+end
+
+haystack = Haystack.new
+haystack.validateNeedle
