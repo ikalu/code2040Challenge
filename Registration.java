a92a15e6be220b5b26316453956d8cc9e3cda56d
Ifeanyi <ikalu13@gmail.com>
2014-12-15 01:47:12 -0500

    Add more instance variables

--- Registration.java
+++ Registration.java
@@ -6,6 +6,8 @@ public class Registration {
 
 	private String email;
 	private String github;
+	private String token;
+	private String string;
 	
 	public void setEmail(String email){
 		this.email = email;
@@ -15,11 +17,27 @@ public class Registration {
 		this.github = github;
 	}
 	
+	public void setToken(String token){
+		this.token = token;
+	}
+	
+	public void setString(String string){
+		this.string = string;
+	}
+	
 	public String getEmail(){
 		return email;
 	}
 	
 	public String getGithub(){
 		return github;
+	}
+	
+	public String getToken(){
+		return token;
+	}
+	
+	public String getString(){
+		return string;
 	}
 }
