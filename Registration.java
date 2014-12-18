7c32e95a6f13758444ab7eda0fa0380bb94fb397
Ifeanyi <ikalu13@gmail.com>
2014-12-17 23:45:37 -0500

    Add more instance variables
    * Contains private instance variables
    * All the four classes/stages use this class

--- Registration.java
+++ Registration.java
@@ -8,6 +8,9 @@ public class Registration {
 	private String github;
 	private String token;
 	private String string;
+	private Object needle;
+	private Object[] array;
+	private String datestamp;
 	
 	public void setEmail(String email){
 		this.email = email;
@@ -25,6 +28,18 @@ public class Registration {
 		this.string = string;
 	}
 	
+	public void setNeedle(Object needle){
+		this.needle = needle;
+	}
+	
+	public void setArray(Object[] array){
+		this.array = array;
+	}
+	
+	public void setDatestamp(String datestamp){
+		this.datestamp = datestamp;
+	}
+	
 	public String getEmail(){
 		return email;
 	}
@@ -39,5 +54,17 @@ public class Registration {
 	
 	public String getString(){
 		return string;
+	}
+	
+	public Object getNeedle(){
+		return needle;
+	}
+	
+	public Object[] getArray(){
+		return array;
+	}
+	
+	public String getDatestamp(){
+		return datestamp;
 	}
 }
